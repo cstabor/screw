c09b0a86824c24d05d05148f6ba775e9ef5e9898
cstabor <cstabor@hotmail.com>
2016-05-11 23:09:05 +0800

    update

--- src/helper.php
+++ src/helper.php
@@ -1,9 +1,16 @@
 <?php
+function println($var) {
 
-function  println($var) {
-	print_r($var);
-}
-
-function println2($var) {
-		print_r($var);
+		if (PHP_SAPI == 'cli' || (isset($_SERVER['HTTP_USER_AGENT']) && strpos($_SERVER['HTTP_USER_AGENT'], 'curl') !== false)) {
+				if (is_scalar($var)) {
+						$ret = $var.PHP_EOL;
+				} else {
+						$ret = print_r($var, true);
+				}
+		} else {
+				$ret = "<pre>";
+				$ret .= print_r($var, true);
+				$ret .= "</pre>";
+		}
+		echo $ret;
 }
