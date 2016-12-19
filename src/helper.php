95038b7347fb3b9156c727c00f9ddf55d28c38dc
cstabor <cstabor@hotmail.com>
2016-12-19 09:45:09 +0800

    update

--- src/helper.php
+++ src/helper.php
@@ -29,3 +29,13 @@ function println($var, $return = false) {
 		return $val;
 	}
 }
+
+function array_random($arr, $num = 1) {
+    shuffle($arr);
+
+    $r = array();
+    for ($i = 0; $i < $num; $i++) {
+        $r[] = $arr[$i];
+    }
+    return $num == 1 ? $r[0] : $r;
+}
