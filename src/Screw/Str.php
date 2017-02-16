641448f41c844e637ad6f556cf5d76d6a5c93091
cstabor <cstabor@hotmail.com>
2017-02-16 13:08:47 +0800

    update incdent to 4 whitespace

--- src/Screw/Str.php
+++ src/Screw/Str.php
@@ -4,168 +4,168 @@ namespace Screw;
 
 class Str
 {
-    /**
-     * 生成指定长度的随机字符串(包含大写英文字母, 小写英文字母, 数字)
-     *
-     * @param int $length 需要生成的字符串的长度
-     * @return string 包含 大小写英文字母 和 数字 的随机字符串
-     */
-    public static function randomAlphabetAndDigit($length)
-    {
-        $charsSpace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        return self::randomByCharsSpace($length, $charsSpace);
-    }
-
-    /**
-     * @param $length
-     * @param $charsSpace
-     * @return string
-     */
-    public static function randomByCharsSpace($length, $charsSpace)
-    {
-        $res = '';
-        for ($i = 0; $i < $length; $i++) {
-            $index = mt_rand(0, strlen($charsSpace) - 1);
-            $res .= $charsSpace[$index];
-        }
-        return $res;
-    }
-
-    /**
-     * get rand visual char string
-     * @param $length
-     *
-     * @return string
-     */
-    public static function random($length) {
-	$res = '';
-	for ($i = 0; $i < $length; $i++) {
-		$res .= chr(mt_rand(33, 126));
+	/**
+	 * 生成指定长度的随机字符串(包含大写英文字母, 小写英文字母, 数字)
+	 *
+	 * @param int $length 需要生成的字符串的长度
+	 * @return string 包含 大小写英文字母 和 数字 的随机字符串
+	 */
+	public static function randomAlphabetAndDigit($length)
+	{
+		$charsSpace = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+		return self::randomByCharsSpace($length, $charsSpace);
 	}
-	return $res;
-    }
 
-    /**
-     * 特殊字符转换为全角
-     *
-     * @param $str
-     * @return string
-     */
-    public static function specialCharsToSBC($str)
-    {
-        /**
-         * '０' => '0', '１' => '1', '２' => '2', '３' => '3', '４' => '4',
-         * '５' => '5', '６' => '6', '７' => '7', '８' => '8', '９' => '9',
-         * 'Ａ' => 'A', 'Ｂ' => 'B', 'Ｃ' => 'C', 'Ｄ' => 'D', 'Ｅ' => 'E',
-         * 'Ｆ' => 'F', 'Ｇ' => 'G', 'Ｈ' => 'H', 'Ｉ' => 'I', 'Ｊ' => 'J',
-         * 'Ｋ' => 'K', 'Ｌ' => 'L', 'Ｍ' => 'M', 'Ｎ' => 'N', 'Ｏ' => 'O',
-         * 'Ｐ' => 'P', 'Ｑ' => 'Q', 'Ｒ' => 'R', 'Ｓ' => 'S', 'Ｔ' => 'T',
-         * 'Ｕ' => 'U', 'Ｖ' => 'V', 'Ｗ' => 'W', 'Ｘ' => 'X', 'Ｙ' => 'Y',
-         * 'Ｚ' => 'Z', 'ａ' => 'a', 'ｂ' => 'b', 'ｃ' => 'c', 'ｄ' => 'd',
-         * 'ｅ' => 'e', 'ｆ' => 'f', 'ｇ' => 'g', 'ｈ' => 'h', 'ｉ' => 'i',
-         * 'ｊ' => 'j', 'ｋ' => 'k', 'ｌ' => 'l', 'ｍ' => 'm', 'ｎ' => 'n',
-         * 'ｏ' => 'o', 'ｐ' => 'p', 'ｑ' => 'q', 'ｒ' => 'r', 'ｓ' => 's',
-         * 'ｔ' => 't', 'ｕ' => 'u', 'ｖ' => 'v', 'ｗ' => 'w', 'ｘ' => 'x',
-         * 'ｙ' => 'y', 'ｚ' => 'z',
-         */
+	/**
+	 * @param $length
+	 * @param $charsSpace
+	 * @return string
+	 */
+	public static function randomByCharsSpace($length, $charsSpace)
+	{
+		$res = '';
+		for ($i = 0; $i < $length; $i++) {
+			$index = mt_rand(0, strlen($charsSpace) - 1);
+			$res .= $charsSpace[$index];
+		}
+		return $res;
+	}
 
-        $arr = [
-            '（' => '(', '）' => ')', '〔' => '[', '〕' => ']', '【' => '[',
-            '】' => ']', '〖' => '[', '〗' => ']', '“' => '[', '”' => ']',
-            '‘' => '[', '’' => ']', '｛' => '{', '｝' => '}', '《' => '<',
-            '》' => '>',
-            '％' => '%', '＋' => '+', '—' => '-', '－' => '-', '～' => '-',
-            '：' => ':', '。' => '.', '、' => ',', '，' => '.', '、' => '.',
-            '；' => ',', '？' => '?', '！' => '!', '…' => '-', '‖' => '|',
-            '”' => '"', '’' => '`', '‘' => '`', '｜' => '|', '〃' => '"',
-            '　' => ' ', '＄' => '$', '＠' => '@', '＃' => '#', '＾' => '^', '＆' => '&', '＊' => '*',
-            '＂' => '"'];
+	/**
+	 * get rand visual char string
+	 * @param $length
+	 *
+	 * @return string
+	 */
+	public static function random($length) {
+		$res = '';
+		for ($i = 0; $i < $length; $i++) {
+			$res .= chr(mt_rand(33, 126));
+		}
+		return $res;
+	}
 
-        return strtr($str, array_flip($arr));
-    }
+	/**
+	 * 特殊字符转换为全角
+	 *
+	 * @param $str
+	 * @return string
+	 */
+	public static function specialCharsToSBC($str)
+	{
+		/**
+		 * '０' => '0', '１' => '1', '２' => '2', '３' => '3', '４' => '4',
+		 * '５' => '5', '６' => '6', '７' => '7', '８' => '8', '９' => '9',
+		 * 'Ａ' => 'A', 'Ｂ' => 'B', 'Ｃ' => 'C', 'Ｄ' => 'D', 'Ｅ' => 'E',
+		 * 'Ｆ' => 'F', 'Ｇ' => 'G', 'Ｈ' => 'H', 'Ｉ' => 'I', 'Ｊ' => 'J',
+		 * 'Ｋ' => 'K', 'Ｌ' => 'L', 'Ｍ' => 'M', 'Ｎ' => 'N', 'Ｏ' => 'O',
+		 * 'Ｐ' => 'P', 'Ｑ' => 'Q', 'Ｒ' => 'R', 'Ｓ' => 'S', 'Ｔ' => 'T',
+		 * 'Ｕ' => 'U', 'Ｖ' => 'V', 'Ｗ' => 'W', 'Ｘ' => 'X', 'Ｙ' => 'Y',
+		 * 'Ｚ' => 'Z', 'ａ' => 'a', 'ｂ' => 'b', 'ｃ' => 'c', 'ｄ' => 'd',
+		 * 'ｅ' => 'e', 'ｆ' => 'f', 'ｇ' => 'g', 'ｈ' => 'h', 'ｉ' => 'i',
+		 * 'ｊ' => 'j', 'ｋ' => 'k', 'ｌ' => 'l', 'ｍ' => 'm', 'ｎ' => 'n',
+		 * 'ｏ' => 'o', 'ｐ' => 'p', 'ｑ' => 'q', 'ｒ' => 'r', 'ｓ' => 's',
+		 * 'ｔ' => 't', 'ｕ' => 'u', 'ｖ' => 'v', 'ｗ' => 'w', 'ｘ' => 'x',
+		 * 'ｙ' => 'y', 'ｚ' => 'z',
+		 */
+
+		$arr = [
+			'（' => '(', '）' => ')', '〔' => '[', '〕' => ']', '【' => '[',
+			'】' => ']', '〖' => '[', '〗' => ']', '“' => '[', '”' => ']',
+			'‘' => '[', '’' => ']', '｛' => '{', '｝' => '}', '《' => '<',
+			'》' => '>',
+			'％' => '%', '＋' => '+', '—' => '-', '－' => '-', '～' => '-',
+			'：' => ':', '。' => '.', '、' => ',', '，' => '.', '、' => '.',
+			'；' => ',', '？' => '?', '！' => '!', '…' => '-', '‖' => '|',
+			'”' => '"', '’' => '`', '‘' => '`', '｜' => '|', '〃' => '"',
+			'　' => ' ', '＄' => '$', '＠' => '@', '＃' => '#', '＾' => '^', '＆' => '&', '＊' => '*',
+			'＂' => '"'];
+
+		return strtr($str, array_flip($arr));
+	}
 
 
-    /**
-     * 将unicode转换成字符
-     *
-     * @param int $unicode
-     * @return string UTF-8字符
-     **/
-    public static function unicode2Char($unicode)
-    {
-        if ($unicode < 128) return chr($unicode);
-        if ($unicode < 2048) return chr(($unicode >> 6) + 192) .
-        chr(($unicode & 63) + 128);
-        if ($unicode < 65536) return chr(($unicode >> 12) + 224) .
-        chr((($unicode >> 6) & 63) + 128) .
-        chr(($unicode & 63) + 128);
-        if ($unicode < 2097152) return chr(($unicode >> 18) + 240) .
-        chr((($unicode >> 12) & 63) + 128) .
-        chr((($unicode >> 6) & 63) + 128) .
-        chr(($unicode & 63) + 128);
-        return false;
-    }
+	/**
+	 * 将unicode转换成字符
+	 *
+	 * @param int $unicode
+	 * @return string UTF-8字符
+	 **/
+	public static function unicode2Char($unicode)
+	{
+		if ($unicode < 128) return chr($unicode);
+		if ($unicode < 2048) return chr(($unicode >> 6) + 192) .
+			chr(($unicode & 63) + 128);
+		if ($unicode < 65536) return chr(($unicode >> 12) + 224) .
+			chr((($unicode >> 6) & 63) + 128) .
+			chr(($unicode & 63) + 128);
+		if ($unicode < 2097152) return chr(($unicode >> 18) + 240) .
+			chr((($unicode >> 12) & 63) + 128) .
+			chr((($unicode >> 6) & 63) + 128) .
+			chr(($unicode & 63) + 128);
+		return false;
+	}
 
-    /**
-     * 将字符转换成unicode
-     *
-     * @param string $char 必须是UTF-8字符
-     * @return int
-     **/
-    public static function char2Unicode($char)
-    {
-        switch (strlen($char)) {
-            case 1 :
-                return ord($char);
-            case 2 :
-                return (ord($char{1}) & 63) |
-                ((ord($char{0}) & 31) << 6);
-            case 3 :
-                return (ord($char{2}) & 63) |
-                ((ord($char{1}) & 63) << 6) |
-                ((ord($char{0}) & 15) << 12);
-            case 4 :
-                return (ord($char{3}) & 63) |
-                ((ord($char{2}) & 63) << 6) |
-                ((ord($char{1}) & 63) << 12) |
-                ((ord($char{0}) & 7) << 18);
-            default :
-                trigger_error('Character is not UTF-8!', E_USER_WARNING);
-                return false;
-        }
-    }
+	/**
+	 * 将字符转换成unicode
+	 *
+	 * @param string $char 必须是UTF-8字符
+	 * @return int
+	 **/
+	public static function char2Unicode($char)
+	{
+		switch (strlen($char)) {
+		case 1 :
+			return ord($char);
+		case 2 :
+			return (ord($char{1}) & 63) |
+				((ord($char{0}) & 31) << 6);
+		case 3 :
+			return (ord($char{2}) & 63) |
+				((ord($char{1}) & 63) << 6) |
+				((ord($char{0}) & 15) << 12);
+		case 4 :
+			return (ord($char{3}) & 63) |
+				((ord($char{2}) & 63) << 6) |
+				((ord($char{1}) & 63) << 12) |
+				((ord($char{0}) & 7) << 18);
+		default :
+			trigger_error('Character is not UTF-8!', E_USER_WARNING);
+			return false;
+		}
+	}
 
-    /**
-     * 半角转全角
-     *
-     * @param string $str
-     * @return string
-     **/
-    public static function dbc2Sbc($str)
-    {
-        return preg_replace(
-        // 半角字符
-            '/[x{0020}x{0020}-x{7e}]/ue',
-            // 编码转换
-            // 0x0020是空格，特殊处理，其他半角字符编码+0xfee0即可以转为全角
-            '($unicode=char2Unicode(\'\0\')) == 0x0020 ? unicode2Char（0x3000） : (($code=$unicode+0xfee0) > 256 ? unicode2Char($code) : chr($code))'
-            , $str);
-    }
+	/**
+	 * 半角转全角
+	 *
+	 * @param string $str
+	 * @return string
+	 **/
+	public static function dbc2Sbc($str)
+	{
+		return preg_replace(
+			// 半角字符
+			'/[x{0020}x{0020}-x{7e}]/ue',
+			// 编码转换
+			// 0x0020是空格，特殊处理，其他半角字符编码+0xfee0即可以转为全角
+			'($unicode=char2Unicode(\'\0\')) == 0x0020 ? unicode2Char（0x3000） : (($code=$unicode+0xfee0) > 256 ? unicode2Char($code) : chr($code))'
+			, $str);
+	}
 
-    /**
-     * 全角转半角
-     *
-     * @param string $str
-     * @return string
-     **/
-    function sbc2Dbc($str)
-    {
-        return preg_replace(
-        // 全角字符
-            '/[x{3000}x{ff01}-x{ff5f}]/ue',
-            // 编码转换
-            // 0x3000是空格，特殊处理，其他全角字符编码-0xfee0即可以转为半角
-            '($unicode=char2Unicode(\'\0\')) == 0x3000 ? " " : (($code=$unicode-0xfee0) > 256 ? unicode2Char($code) : chr($code))'
-            , $str);
-    }
+	/**
+	 * 全角转半角
+	 *
+	 * @param string $str
+	 * @return string
+	 **/
+	function sbc2Dbc($str)
+	{
+		return preg_replace(
+			// 全角字符
+			'/[x{3000}x{ff01}-x{ff5f}]/ue',
+			// 编码转换
+			// 0x3000是空格，特殊处理，其他全角字符编码-0xfee0即可以转为半角
+			'($unicode=char2Unicode(\'\0\')) == 0x3000 ? " " : (($code=$unicode-0xfee0) > 256 ? unicode2Char($code) : chr($code))'
+			, $str);
+	}
 }
--- src/helper.php
+++ src/helper.php
@@ -9,19 +9,19 @@
  */
 function println($var, $return = false) {
 	if (PHP_SAPI === 'cli' || (isset ( $_SERVER ['HTTP_USER_AGENT'] )
-            && strpos ( $_SERVER ['HTTP_USER_AGENT'], 'curl' ) !== false)) {
-		if (is_scalar ( $var )) {
-			$val = $var . PHP_EOL;
-		} else {
-			$val = print_r($var, true);
-			if (!$val) {
-				$val .= PHP_EOL;
+		&& strpos ( $_SERVER ['HTTP_USER_AGENT'], 'curl' ) !== false)) {
+			if (is_scalar ( $var )) {
+				$val = $var . PHP_EOL;
+			} else {
+				$val = print_r($var, true);
+				if (!$val) {
+					$val .= PHP_EOL;
+				}
 			}
+		} else {
+			$val = '<pre>'.print_r($var, true).'</pre>';
 		}
-	} else {
-		$val = '<pre>'.print_r($var, true).'</pre>';
-	}
-	
+
 	if (!$return) {
 		echo $val;
 	} else {
@@ -37,12 +37,12 @@ function println($var, $return = false) {
  * @return string|array
  */
 function array_random($arr, $num = 1) {
-    shuffle($arr);
-    $r = [];
-    for ($i = 0; $i < $num; $i++) {
-        $r[] = $arr[$i];
-    }
-    return $num === 1 ? $r[0] : $r;
+	shuffle($arr);
+	$r = [];
+	for ($i = 0; $i < $num; $i++) {
+		$r[] = $arr[$i];
+	}
+	return $num === 1 ? $r[0] : $r;
 }
 
 /**
@@ -52,7 +52,7 @@ function array_random($arr, $num = 1) {
  * @return string
  */
 function array_random_value($arr) {
-    return $arr[array_rand($arr)];
+	return $arr[array_rand($arr)];
 }
 
 /**
@@ -61,56 +61,56 @@ function array_random_value($arr) {
  * $param object $object
  */
 function object_array($object) {
-    $public = [];
-    $reflection = new ReflectionClass($object);
-    foreach ($reflection->getProperties() as $property) {
-        $property->setAccessible(true);
-        if (is_object($property->getValue($object))) {
-            $public[$property->getName()] = call_user_func_array(__FUNCTION__, [$property->getValue($object)]);
-        } else {
-            $public[$property->getName()] = $property->getValue($object);
-        }
-    }
-    return $public;
+	$public = [];
+	$reflection = new ReflectionClass($object);
+	foreach ($reflection->getProperties() as $property) {
+		$property->setAccessible(true);
+		if (is_object($property->getValue($object))) {
+			$public[$property->getName()] = call_user_func_array(__FUNCTION__, [$property->getValue($object)]);
+		} else {
+			$public[$property->getName()] = $property->getValue($object);
+		}
+	}
+	return $public;
 }
 
 /**
  * only support public property
  */
 function object_to_array($d) {
-    if (is_object($d)) {
-        // Gets the properties of the given object
-        // with get_object_vars function
-        $d = get_object_vars($d);
-    }
+	if (is_object($d)) {
+		// Gets the properties of the given object
+		// with get_object_vars function
+		$d = get_object_vars($d);
+	}
 
-    if (is_array($d)) {
-        /*
-         * Return array converted to object
-         * Using __FUNCTION__ (Magic constant)
-         * for recursive call
-         */
-        return array_map(__FUNCTION__, $d);
-    } else {
-        // Return array
-        return $d;
-    }
+	if (is_array($d)) {
+		/*
+		 * Return array converted to object
+		 * Using __FUNCTION__ (Magic constant)
+		 * for recursive call
+		 */
+		return array_map(__FUNCTION__, $d);
+	} else {
+		// Return array
+		return $d;
+	}
 }
 
 function camel_to_underscore($name) {
-    $temp_array = [];
-    for($i=0;$i<strlen($name);$i++){
-        $ascii_code = ord($name[$i]);
-        if($ascii_code >= 65 && $ascii_code <= 90){
-            if($i == 0){
-                $temp_array[] = chr($ascii_code + 32);
-            }else{
-                $temp_array[] = '_'.chr($ascii_code + 32);
-            }
-        }else{
-            $temp_array[] = $name[$i];
-        }
-    }
-    return implode('',$temp_array);
+	$temp_array = [];
+	for($i=0;$i<strlen($name);$i++){
+		$ascii_code = ord($name[$i]);
+		if($ascii_code >= 65 && $ascii_code <= 90){
+			if($i == 0){
+				$temp_array[] = chr($ascii_code + 32);
+			}else{
+				$temp_array[] = '_'.chr($ascii_code + 32);
+			}
+		}else{
+			$temp_array[] = $name[$i];
+		}
+	}
+	return implode('',$temp_array);
 }
 
