7c57efa547d103a8123e533a4d976412d46d9543
cstabor <cstabor@hotmail.com>
2017-10-24 10:19:55 +0800

    update indent

--- src/helper.php
+++ src/helper.php
@@ -2,31 +2,32 @@
 
 /**
  * print a mixed value to screen or return
- * 
+ *
  * @param mixed $var
  * @param boolean $return
  * @return string
  */
-function println($var, $return = false) {
-	if (PHP_SAPI === 'cli' || (isset ( $_SERVER ['HTTP_USER_AGENT'] )
-		&& strpos ( $_SERVER ['HTTP_USER_AGENT'], 'curl' ) !== false)) {
-			if (is_scalar ( $var )) {
-				$val = $var . PHP_EOL;
-			} else {
-				$val = print_r($var, true);
-				if (!$val) {
-					$val .= PHP_EOL;
-				}
-			}
-		} else {
-			$val = '<pre>'.print_r($var, true).'</pre>';
-		}
-
-	if (!$return) {
-		echo $val;
-	} else {
-		return $val;
-	}
+function println($var, $return = false)
+{
+    
+    // (isset ($_SERVER ['HTTP_USER_AGENT']) && strpos($_SERVER ['HTTP_USER_AGENT'], 'curl') !== false)
+    if (PHP_SAPI === 'cli') {
+        if (is_scalar($var)) {
+            $val = sprintf("%s: %s".PHP_EOL, gettype($var), $var);
+        } else {
+            $val = print_r($var, true);
+            if (!$val) {
+                $val .= PHP_EOL;
+            }
+        }
+    } else {
+        $val = '<pre>' . print_r($var, true) . '</pre>';
+    }
+    
+    if ($return) {
+        return $val;
+    }
+    echo $val;
 }
 
 /**
@@ -36,13 +37,14 @@ function println($var, $return = false) {
  * @param int $num
  * @return string|array
  */
-function array_random($arr, $num = 1) {
-	shuffle($arr);
-	$r = [];
-	for ($i = 0; $i < $num; $i++) {
-		$r[] = $arr[$i];
-	}
-	return $num === 1 ? $r[0] : $r;
+function array_random($arr, $num = 1)
+{
+    shuffle($arr);
+    $r = [];
+    for ($i = 0; $i < $num; $i++) {
+        $r[] = $arr[$i];
+    }
+    return $num === 1 ? $r[0] : $r;
 }
 
 /**
@@ -51,16 +53,17 @@ function array_random($arr, $num = 1) {
  * @param object $object
  * @return array
  */
-function object_array($object) {
-	$public = [];
-	$reflection = new ReflectionClass($object);
-	foreach ($reflection->getProperties() as $property) {
-		$property->setAccessible(true);
-		if (is_object($property->getValue($object))) {
-			$public[$property->getName()] = call_user_func_array(__FUNCTION__, [$property->getValue($object)]);
-		} else {
-			$public[$property->getName()] = $property->getValue($object);
-		}
-	}
-	return $public;
+function object_array($object)
+{
+    $public = [];
+    $reflection = new ReflectionClass($object);
+    foreach ($reflection->getProperties() as $property) {
+        $property->setAccessible(true);
+        if (is_object($property->getValue($object))) {
+            $public[$property->getName()] = call_user_func_array(__FUNCTION__, [$property->getValue($object)]);
+        } else {
+            $public[$property->getName()] = $property->getValue($object);
+        }
+    }
+    return $public;
 }
